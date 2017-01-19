2d8e685ed49b44dc0fbbac89e4163ac6b40b5d95
Michael Mouawad <michael_mouawad@aol.com>
2017-01-19 23:57:12 +0200

    updated common header

--- common.h
+++ common.h
@@ -1,1 +1,19 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#ifdef __unix__
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+#endif
+
 void errexit(const char* message);
+
+#endif

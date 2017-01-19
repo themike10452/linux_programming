0a0694581bfd884f8bb9d8a763d06e9e9fe69470
Michael Mouawad <michael_mouawad@aol.com>
2017-01-19 22:49:53 +0200

    updated vector library

--- vector.c
+++ vector.c
@@ -4,39 +4,81 @@
 #include <assert.h>
 #include "vector.h"
 
-vec* vector(unsigned int elemsize, unsigned int capacity)
+vec* vector(unsigned int capacity)
 {
 	vec* vec = malloc(sizeof(vec));
 	if (!vec) return NULL;
 
-	vec->_data = malloc(elemsize * capacity);
+	vec->_data = malloc(sizeof(void*) * capacity);
 	if (!vec->_data) return NULL;
 
-	vec->_elemsize = elemsize;
 	vec->size = 0;
 	vec->capacity = capacity;
 
 	return vec;
 }
 
+void vector_resize(vec* vec, unsigned int capacity)
+{
+	void** data = realloc(vec->_data, sizeof(void*) * capacity);
+	if (data)
+	{
+		vec->_data = data;
+		vec->capacity = capacity;
+	}
+}
+
 void* vector_get(vec* vec, unsigned int index)
 {
 	assert(index < vec->size);
-	return (void*)*(vec->_data + (index * vec->_elemsize));
+	return vec->_data[index];
 }
 
-int vector_getp(vec* vec, void* out, unsigned int index)
+void vector_push(vec* vec, void* element)
 {
-	if (index >= vec->size) return -1;
-	memcpy((int*)out, vec->_data + (index * vec->_elemsize), vec->_elemsize);
+	if (vec->size == vec->capacity)
+		vector_resize(vec, vec->capacity * 2);
+		
+	vec->_data[vec->size++] = element;
+}
+
+int vector_remove(vec* vec, void* element)
+{
+	int index = vector_find(vec, element);
+	if (index > -1)
+	{
+		vector_removeat(vec, index);
+		return 1;
+	}
+	
 	return 0;
 }
 
-void vector_push(vec* vec, void* element, unsigned int elemsize)
+void vector_removeat(vec* vec, unsigned int index)
 {
-	assert(elemsize <= vec->_elemsize);
-	memcpy(vec->_data + (vec->size * vec->_elemsize), element, vec->_elemsize);
-	vec->size++;
+	assert(index < vec->size);
+
+	vec->_data[index] = NULL;
+
+	for (int i = index; i < vec->size - 1; i++)
+	{
+		vec->_data[i] = vec->_data[i + 1];
+	}
+
+	vec->size--;
+	if (vec->size > 0 && vec->size == vec->capacity / 4)
+	{
+		vector_resize(vec, vec->capacity / 2);
+	}
+}
+
+int vector_find(vec* vec, void* element)
+{
+	for (int i = 0; i < vec->size; i++)
+		if (element == vec->_data[i])
+			return i;
+
+	return -1;
 }
 
 void vector_free(vec* vec)
--- vector.h
+++ vector.h
@@ -1,16 +1,21 @@
 typedef struct dynamic_array {
 	unsigned int size;
 	unsigned int capacity;
-	unsigned int _elemsize;
-	int* _data;
+	void** _data;
 } vec;
 
-vec* vector(unsigned int elemsize, unsigned int capacity);
+vec* vector(unsigned int capacity);
+
+void vector_resize(vec* vec, unsigned int capacity);
 
 void* vector_get(vec* vec, unsigned int index);
 
-int vector_getp(vec* vec, void* out, unsigned int index);
+void vector_push(vec* vec, void* element);
 
-void vector_push(vec* vec, void* element, unsigned int elemsize);
+int vector_remove(vec* vec, void* element);
+
+void vector_removeat(vec* vec, unsigned int index);
+
+int vector_find(vec* vec, void* element);
 
 void vector_free(vec* vec);

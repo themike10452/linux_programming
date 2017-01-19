08df9b89a842d220b682fdb79f5197c5adbe7037
Michael Mouawad <michael_mouawad@aol.com>
2017-01-19 23:57:22 +0200

    server update

--- server.c
+++ server.c
@@ -1,13 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include "common.h"
-#include "vector.h"
 #include "server.h"
 
 vec *vec_cli_infos;
@@ -16,7 +6,7 @@ int main(int argc, char** argv)
 {
     if (argc < 2)
     {
-        errexit("No port number provided.\n");
+        errexit("No port number provided\n");
     }
 
     int port = atoi(argv[1]);
@@ -34,7 +24,7 @@ int main(int argc, char** argv)
     errno = bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
     if (errno < 0)
     {
-        errexit("Binding error.\n");
+        errexit("Binding error\n");
     }
 
     vec_cli_infos = vector(64);
@@ -69,38 +59,69 @@ void* serv_initconn(void* args)
 {
     struct client_info* cli_info = args;
 
-    vector_push(vec_cli_infos, cli_info);
+    printf("%d is connected\n", cli_info->sockfd);
 
     char readbuffer[SERV_BUFF_READ], writebuffer[SERV_BUFF_WRITE];
     bzero(readbuffer, sizeof(readbuffer));
     bzero(writebuffer, sizeof(writebuffer));
 
-    printf("%d is connected.\n", cli_info->sockfd);
+    sprintf(writebuffer, "%s\n%s: ", SERV_MSG_WELCOME, SERV_MSC_USERNAME);
+    write(cli_info->sockfd, writebuffer, strlen(writebuffer));
 
-    strcpy(writebuffer, SERV_MSG_WELCOME);
-    write(cli_info->sockfd, writebuffer, strlen(SERV_MSG_WELCOME));
-
-    int readlen;
+    int readlen, firstread = 1;
     while ((readlen = read(cli_info->sockfd, &readbuffer, SERV_BUFF_READ)))
     {
         if (readlen > 1)
         {
-            printf("%d>%s", cli_info->sockfd, readbuffer);
-            serv_broadcast(cli_info->sockfd, readbuffer);
+            if (firstread)
+            {
+                strncpy(cli_info->cliname, readbuffer, readlen - 1);
+                printf("%d -> %s\n", cli_info->sockfd, cli_info->cliname);
+
+                sprintf(writebuffer, "%s is now online\n", cli_info->cliname);
+                cli_broadcast(NULL, writebuffer);
+                vector_push(vec_cli_infos, cli_info);
+
+                firstread = !firstread;
+            } 
+            else 
+            {
+                printf("%s > %s", cli_info->cliname, readbuffer);
+                cli_broadcast(cli_info, readbuffer);
+            }
+            
             bzero(readbuffer, sizeof(readbuffer));
         }
     }
 
-    close(cli_info->sockfd);
+    printf("%d is disconnected\n", cli_info->sockfd);
 
-    printf("%d is disconnected.\n", cli_info->sockfd);
+    if (vector_remove(vec_cli_infos, cli_info))
+    {
+        bzero(writebuffer, sizeof(writebuffer));
+        sprintf(writebuffer, "%s is disconnected.\n", cli_info->cliname);
+        cli_broadcast(NULL, writebuffer);
+    }
+
+    close(cli_info->sockfd);
 }
 
-void serv_broadcast(int src_sockfd, const char* message)
+void cli_broadcast(struct client_info* cli_info, const char* message)
 {
     char text[SERV_BUFF_WRITE];
     bzero(text, sizeof(text));
-    sprintf(text, "%d says: %s", src_sockfd, message);
+
+#ifdef __unix__
+    if (cli_info)
+        sprintf(text, KYEL "%s says:" KNRM " %s", cli_info->cliname, message);
+    else
+        sprintf(text, KBLU "ADMIN: %s" KNRM, message);
+#else
+    if (cli_info)
+        sprintf(text, "%s says: %s", cli_info->cliname, message);
+    else
+        sprintf(text, "ADMIN: %s", message);
+#endif
 
     struct client_info* ci;
     int i, count = vec_cli_infos->size;
@@ -109,7 +130,7 @@ void serv_broadcast(int src_sockfd, const char* message)
     {
         ci = (struct client_info*)vector_get(vec_cli_infos, i);
 
-        if (ci->sockfd != src_sockfd)
+        if (!cli_info || ci->sockfd != cli_info->sockfd)
         {
             write(ci->sockfd, text, strlen(text));
         }
--- server.h
+++ server.h
@@ -1,17 +1,34 @@
-// macros
+#ifndef SERVER_H
+#define SERVER_H
+
+// constants
 #define SERV_BUFF_READ 512
 #define SERV_BUFF_WRITE 512
 
 #define SERV_MSG_WELCOME "Welcome!\n"
-// .macros
+#define SERV_MSC_USERNAME "Please select your username"
+// .constants
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "common.h"
+#include "vector.h"
 
 struct client_info
 {
 	struct sockaddr_in cliaddr;
 	int cliaddrlen;
 	int sockfd;
-	char* cliname;
+	char cliname[32];
 };
 
 void* serv_initconn(void* args);
-void serv_broadcast(int src_sockfd, const char* message);
+void cli_broadcast(struct client_info* cli_info, const char* message);
+
+#endif
